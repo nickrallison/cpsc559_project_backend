b770a3e89339374a6c994eaec37027d44699b36c
Nick Allison <nickrallison1@gmail.com>
2025-02-17 16:39:06 -0700

    added python script to test interact with application

--- scripts/test_script.py
+++ scripts/test_script.py
@@ -1,41 +1,41 @@
 import requests
 import json
 
+
 class StoredObject:
-    def __init__(self, unix_millis, user_id, data):
-        self.id = None
-        self.unix_millis = unix_millis
+    def __init__(self, user_id, user_message_id, data):
         self.user_id = user_id
+        self.user_message_id = user_message_id
         self.data = data
-
-    def __str__(self):
-        return f'id: {self.id}, unix_millis: {self.unix_millis}, user_id: {self.user_id}, data: {self.data}'
-
-    def __repr__(self):
-        return f'id: {self.id}, unix_millis: {self.unix_millis}, user_id: {self.user_id}, data: {self.data}'
 
     def __dict__(self):
         return {
-            'id': self.id,
-            'unix_millis': self.unix_millis,
             'user_id': self.user_id,
+            'user_message_id': self.user_message_id,
             'data': self.data
         }
 
+
+# Test script to create some objects and print them
+# Run with `python scripts/test_script.py`
 def main():
+    userId = 1
+    url = f'http://localhost:8080/objects?userId={userId}'
+    response = requests.get(url).json()
 
-    url = 'http://localhost:8080/objects'
+    if len(response) == 0:
+        print('No objects found, creating some test objects:')
+        for i in range(3):
+                for j in range(3):
+                    obj = StoredObject(i, j, f'data-{i}-{j}')
+                    response = requests.post(url, json=obj.__dict__())
+                    print(f"Created object: {obj.__dict__()}")
+    else:
+        print('Objects found:')
 
-    # 100 users each created 100 objects
+    for obj in response:
+        print(obj)
 
-    for i in range(100):
-        for j in range(100):
-            obj = StoredObject(id=f'{i}-{j}', unix_millis=0, user_id=i, data=f'{i}-{j}')
-            response = requests.post(url, json=obj.__dict__())
-            print(response.json())
-
-    response = requests.get(url)
-    print(response.json())
 
 if __name__ == '__main__':
     main()
