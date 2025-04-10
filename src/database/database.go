3567c26ca579976c8db473a3710526adc8a2a802
Nick Allison <nickrallison1@gmail.com>
2025-04-10 16:33:44 -0600

    trying to fix db

--- src/database/database.go
+++ src/database/database.go
@@ -16,9 +16,7 @@ type StoredObject struct {
 }
 
 func InitDB(dbPath string) (error, *sql.DB) {
-	//dbPath = dbPath + "?_busy_timeout=5000"
-	println("dbPath:", dbPath)
-	DB, err := sql.Open("libsql", dbPath)
+	DB, err := sql.Open("libsql", "file://"+dbPath+"?_busy_timeout=5000")
 	DB.SetMaxOpenConns(1)
 	if err != nil {
 		return fmt.Errorf("failed to open db %s: %v", dbPath, err), nil
